b922f0fd3a7cc36b4e7d0a1ddcdcaa90eb3aec70
SamDaHanson <samueldavidhanson@gmail.com>
2019-04-29 21:13:46 -0400

    Sam branch (#13)
    * starting backend
    
    * fixed the backend*
    
    * simple testing
    
    * C:/Program Files/Git/data route loads data locally
    
    * fixed package.json bug
    
    * fix the damn package.json
    
    * util functions

--- client/src/pages/SamTest.js
+++ client/src/pages/SamTest.js
@@ -3,100 +3,109 @@ import Container from "../components/Container";
 import Card from "../components/Card";
 import API from "../utils/API";
 
-class SamTest extends Component {
+var state = {
+  events: [],         // All events
+  items: [],          // All items
+  playerItems: [],   // All player items
+  playerStats: []    // Player stats at [0]
+};
 
-  state = {
-    events: []
-  };
-
-  // get all events from /api/events on load
-  componentDidMount() {
-    API.getEvents()
-      .then(res => this.setState(
-        {
-          events: res.data
-        },
-        console.log(res.data)
-        )
-      )
-      .catch(err => console.log(err));
-  }
-
-  // get all events from /api/events
-  loadEvents = () => {
-    API.getEvents()
-      .then(res =>
-        this.setState({ books: res.data })
-      )
-      .catch(err => console.log(err));
-  };
-
-  // saveEvent = req => {
-  //   console.log(req);
-  //   API.saveEvent(req)
-  //     .then(
-  //     )
-  // }
-
-  makeUser = () => {
-    console.log("Events:");
-    console.log(this.state.events);
-    console.log("Making user");
-    var req = {
-      name: "Sam",
-      health: "10",
-      ap: "10",
-      strength: "10",
-      toughness: "10"
-    }
-    API.savePlayer(req)
-      .then();
-
-    API.getPlayers()
-      .then(res =>
-        console.log(res.data)
-      )
-      .catch(err => console.log(err));
+// Exported functions
+// getRandomItem()
+// getRandomEvent()
+// getPlayer()
 
 
-    API.getEvents()
-      .then(res =>
-        console.log(res.data)
-      )
-      .catch(err => console.log(err));
-
-  };
-
-  handleDeleteEvent = id => {
-    API.deleteEvent(id)
-      .then(res => this.loadEvents())
-      .catch(err => console.log(err));
-  }
-
-  render() {
-    return (
-      <Container>
-        <div>
-          <div size="md-12">
-            {this.state.events.length ? (
-              <Card heading="Saved Events">
-                {this.state.events.map(event => (
-                  <h4>{event.title}</h4>
-                ))}
-                </Card>
-            ) : (
-              <Card heading="Saved Events">
-                <h4>None</h4>
-              </Card>
-            )}
-          </div>
-          <button className="btn btn-dark" onClick={this.makeUser} style={{width: "80%", height: "85px", marginRight: "0", fontSize: "30px", marginbottom: "40px"}}>
-            Test
-          </button>
-        </div>
-      </Container>
-    )
-  }
+export function getRandomItem() {
+  var rand = Math.floor(Math.random() * state.items.length);
+  console.log(state.items[rand]);
+  //playerItems.add()
 }
 
-export default SamTest;
+export function getRandomEvent() {
+  var rand = Math.floor(Math.random() * state.events.length);
+  console.log(state.events[rand]);
+}
+
+function getPlayer() {
+  console.log(state.playerStats[0]);
+  return state.playerStats[0];
+}
+
+// Loading functions:
+export { state };
+export function load() {
+  getEverything();
+  setTimeout(
+    function() {
+      getRandomEvent();
+    }
+    .bind(this),
+    5000
+  );
+}
+function getEverything() {
+  setTimeout(
+    function() {
+      getItems();
+    }
+    .bind(this),
+    100
+  );
+  setTimeout(
+    function() {
+      getEvents();
+    }
+    .bind(this),
+    200
+  );
+  setTimeout(
+    function() {
+      getPlayers();
+    }
+    .bind(this),
+    300
+  );
+}
+function storeItems(res) {
+  state.items = res;
+}
+function storeEvents(res) {
+  state.events = res;
+}
+function storePlayers(res) {
+  state.players = res;
+}
+function getItems() {
+  API.getItems()
+    .then(res =>
+      storeItems(res.data)
+    )
+    .catch(err => console.log(err));
+}
+function getEvents() {
+  API.getEvents()
+    .then(res =>
+      storeEvents(res.data)
+    )
+    .catch(err => console.log(err));
+}
+function getPlayers() {
+  API.getPlayers()
+    .then(res =>
+      storePlayers(res.data)
+    )
+    .catch(err => console.log(err));
+}
+
+// Functions needed:
+// -Player Stats Update
+// -Player level up
+// -Attack enemy (regular)
+// -Attack enemy (special)
+// -Enemy attacks (reg/special)
+// -Change location
+// -Get random event
+// -Get random item
+// -Randomizer for each action at each location (x12)
+// -Use item, get and update stats
