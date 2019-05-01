9f3df3612d4f8f33c31c836217fbb21089d184b7
Ignatzz <martinsandeen@gmail.com>
2019-05-01 01:13:48 -0400

    healthbar and ap bar changing styles

--- client/src/components/CombatButtons/index.js
+++ client/src/components/CombatButtons/index.js
@@ -43,6 +43,7 @@ class SpecAttackButton extends React.Component {
 
 class UseItemButton extends React.Component {
   handleClick = () => {
+    utils.openMenu();
    console.log("clickkkk", this)
   }
 
@@ -63,17 +64,18 @@ class PlayerHealthBar extends React.Component {
   state = {
     currentPlayerHealth: 0,
     totalPlayerHealth: 10,
-    percentage: 100
+    healthPercentage: 100
   }
 
   
   componentDidMount() {
     setTimeout(
       function() {
-        console.log("player health percentage: "+ (utils.state.playerStats[0].health)*100/12)
-        console.log("playerstats:" + utils.state.playerStats[0].health);
-        this.setState({currentPlayerHealth:utils.state.playerStats[0].health,
-        percentage: ((utils.state.playerStats[0].health)*100/12)+'%'
+        console.log(utils.state.playerHealth);
+        console.log(utils.state.playerStats[0].health);
+        this.setState({currentPlayerHealth:utils.state.playerHealth,
+          totalPlayerHealth:utils.state.playerStats[0].health,
+          healthPercentage: (utils.state.playerHealth)/(utils.state.playerStats[0].health)*100+'%'
         })
        }
       .bind(this),
@@ -82,15 +84,15 @@ class PlayerHealthBar extends React.Component {
   }
    
   refreshPlayerHealthBar = () => this.setState({ currentPlayerHealth: utils.state.playerStats[0].health, 
-    percentage: ((utils.state.playerStats[0].health)*100/12)
+    healthPercentage: (utils.state.playerHealth)/(utils.state.playerStats[0].health)*100+'%'
   })
 
  
   render() {
     return (
       <div className= "healthBarDiv">
-        <div className= "actualHealth"  style= {{width:this.state.percentage}}>
-        <p className= "healthNumbers">HP: {this.state.currentPlayerHealth} </p>
+        <div className= "actualHealth"  style= {{width:this.state.healthPercentage}}>
+        <p className= "healthNumbers">HP: {this.state.currentPlayerHealth}/{this.state.totalPlayerHealth} </p>
         </div>
 
       </div>
@@ -101,20 +103,39 @@ class PlayerHealthBar extends React.Component {
 
 class PlayerAPBar extends React.Component {
   state = {
-    currentPlayerAP: this.props.currentPlayerAP,
-    totalPlayerAP: this.props.currentPlayerAP
+    currentPlayerAP: 0,
+    totalPlayerAP: 10,
+    APPercentage: 100
 
   };
+  componentDidMount() {
+    setTimeout(
+      function() {
+        console.log(utils.state.playerAP);
+        console.log(utils.state.playerStats[0].ap);
+        this.setState({currentPlayerAP:utils.state.playerAP,
+          totalPlayerHealth:utils.state.playerStats[0].ap,
+          APPercentage: (utils.state.playerAP)/(utils.state.playerStats[0].ap)*100+'%'
+        })
+       }
+      .bind(this),
+      5000
+    ); 
+  }
+   
+  refreshPlayerHealthBar = () => this.setState({ currentPlayerAP: utils.state.playerStats[0].playerAP, 
+    APPercentage: (utils.state.playerAP)/(utils.state.playerStats[0].playerAP)*100+'%'
+  })
 
 render() {
   return (
-    <div className= "APBarDiv">
-      
-      <div className= "actualAP">
-      <p className= "APNumbers">AP: {this.currentPlayerAP} / </p>
-      </div>
+     <div className= "APBarDiv">
+        <div className= "actualAP"  style= {{width:this.state.APPercentage}}>
+        <p className= "APNumbers">AP: {this.state.currentPlayerAP} / {this.state.totalPlayerAP}</p>
+        </div>
 
-    </div>
+      </div>
+    
     
   );
 }
